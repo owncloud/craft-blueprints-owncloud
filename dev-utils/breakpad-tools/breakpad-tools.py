d7dcf771715a49c49b9170683ebf1ece4eb8d6a6
Hannah von Reth <hannah.vonreth@owncloud.com>
2019-11-04 09:36:58 +0100

    Use cmake to build breakpad dump_syms

--- dev-utils/breakpad-tools/breakpad-tools.py
+++ dev-utils/breakpad-tools/breakpad-tools.py
@@ -2,84 +2,29 @@ import info
 import os
 import shutil
 
-from CraftCore import CraftCore
+from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     """We use Jon Turney's fork which adds support for MinGW and a python script to fetch deps"""
     def setTargets(self):
-        for ver in ["pecoff-dwarf-on-git-20171117-fetch-externals-on-win"]:
+        for ver in ["pecoff-dwarf-on-git-20171117", "pecoff-dwarf-on-git-20171117-fetch-externals-on-win"]:
             self.svnTargets[ver] = f"[git]https://github.com/dschmidt/google-breakpad|{ver}|"
+            self.patchToApply[ver] = [("0001-Add-CMake-script-to-build-dump_syms.patch", 1),
+                                       ("Fix-mac-build.patch", 1)]
+        self.svnTargets["master"] = "https://github.com/google/breakpad.git"
+        self.targetSrcSuffix["master"] = "upstream"
+        self.patchToApply["master"] = [("breakpad-tools-20191106.patch", 1)]
 
-        self.defaultTarget = 'pecoff-dwarf-on-git-20171117-fetch-externals-on-win'
         self.description = "The tools part of the breakpad crash-reporting system."
-        self.webpage = "https://github.com/dschmidt/google-breakpad"
+        self.webpage = "https://github.com/jon-turney/google-breakpad"
+        self.defaultTarget = 'pecoff-dwarf-on-git-20171117'
 
     def setDependencies(self):
-        self.buildDependencies["dev-utils/python2"] = "default"
+        self.buildDependencies["dev-utils/python2"] = None
+        self.buildDependencies["virtual/base"] = None
 
-if CraftCore.compiler.isWindows:
-    from Package.MSBuildPackageBase import MSBuildPackageBase
-    
-    class Package(MSBuildPackageBase):
-        def __init__(self):
-            MSBuildPackageBase.__init__(self)
-            self.useShadowBuild = True
-            self.subinfo.options.configure.projectFile = os.path.join('src', 'tools', 'windows', "build_all.vcxproj")
-
-        def fetch(self):
-            if not super().fetch():
-                return False
-
-            utils.system(["python2", "fetch-externals"], cwd=self.sourceDir())
-
-            # Remove binaries that are shipped in source
-            binariesPath = os.path.join(self.sourceDir(), 'src', 'tools', 'windows', 'binaries')
-            if os.path.isdir(binariesPath):
-                shutil.rmtree(binariesPath)
-
-            return True
-
-        def configure(self):
-            gypMain = os.path.join(self.sourceDir(), 'src', 'tools', 'gyp', 'gyp_main.py')
-            gypFile = os.path.join(self.sourceDir(), 'src', 'tools', 'windows', 'tools_windows.gyp')
-
-            # Out of source generation seems broken in gyp with msvs generator, ninja generator failed to generate the project files
-            # There should be no reason to use the one or the other, if ninja works one day and msvs still does not - feel free to switch
-            depth = os.path.join(self.sourceDir(), 'src')
-            cwd = depth
-
-            # WORKAROUND: Visual Studio Detection is broken in containers
-            msvcString = CraftCore.compiler.abi.split("_")[0]
-            msvcVersion = msvcString[4:]
-
-            with utils.ScopedEnv({"GYP_MSVS_VERSION":msvcVersion}):
-                utils.system(['python2', gypMain, '--no-circular-check', gypFile, '-f', 'msvs', '--generator-output=%s' % self.buildDir(), '--depth=%s' % depth], cwd=cwd)
-                MSBuildPackageBase.configure(self)
-            return True
-
-        # def unittest(self):
-            # buildType = 'Release'
-            # dumpSymsUnitTestFile = os.path.join(self.sourceDir(), 'src', 'tools', 'windows', 'dump_syms', 'Release', 'dump_syms_unittest.exe')
-            # utils.system(dumpSymsUnitTestFile)
-
-        def install(self):
-            # 1) symbol testdata is contained in folders named .pdb which causes issues with craft
-            # Should we modify craft to glob only for .pdb files and no folders?
-            # 2) dump_syms testdata contains files that we can't but don't want to install
-            testdataPaths = [os.path.join('src', 'processor', 'testdata'), os.path.join('src', 'tools', 'windows', 'dump_syms', 'testdata')]
-
-            for path in testdataPaths:
-                testSymbolsPath = os.path.join(self.sourceDir(), path)
-                if os.path.isdir(testSymbolsPath):
-                    shutil.rmtree(testSymbolsPath)
-
-            return MSBuildPackageBase.install(self, installHeaders=False)
-
-
-else:
-    from Package.AutoToolsPackageBase import AutoToolsPackageBase
-
-    class Package( AutoToolsPackageBase ):
-        def __init__( self ):
-            AutoToolsPackageBase.__init__( self )
-            self.subinfo.options.configure.args += " --disable-static --enable-shared "
+class Package(CMakePackageBase):
+    def fetch(self):
+        if not super().fetch():
+            return False
+        return utils.system(["python2", "fetch-externals"], cwd=self.sourceDir())
