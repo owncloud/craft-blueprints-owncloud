9acffeb701878f397085b3cb2840ab99f70fdc11
Hannah von Reth <hannah.vonreth@owncloud.com>
2019-11-08 10:43:48 +0100

    Use cmake branch for breakpad

--- dev-utils/breakpad-tools/breakpad-tools.py
+++ dev-utils/breakpad-tools/breakpad-tools.py
@@ -11,13 +11,18 @@ class subinfo(info.infoclass):
             self.svnTargets[ver] = f"[git]https://github.com/dschmidt/google-breakpad|{ver}|"
             self.patchToApply[ver] = [("0001-Add-CMake-script-to-build-dump_syms.patch", 1),
                                        ("Fix-mac-build.patch", 1)]
+
+        for ver in ["cmake"]:
+            self.svnTargets[ver] = f"[git]https://github.com/theonering/google-breakpad|{ver}|"
+            self.targetSrcSuffix[ver] = "theonering"
+
         self.svnTargets["master"] = "https://github.com/google/breakpad.git"
         self.targetSrcSuffix["master"] = "upstream"
         self.patchToApply["master"] = [("breakpad-tools-20191106.patch", 1)]
 
         self.description = "The tools part of the breakpad crash-reporting system."
         self.webpage = "https://github.com/jon-turney/google-breakpad"
-        self.defaultTarget = 'pecoff-dwarf-on-git-20171117'
+        self.defaultTarget = 'cmake'
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/python2"] = None
