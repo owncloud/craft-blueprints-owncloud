e40632d14a7148a1a92e2d720e189c5911527ee3
Hannah von Reth <hannah.vonreth@owncloud.com>
2019-11-19 13:26:51 +0100

    Always pass protobuf_MSVC_STATIC_RUNTIME else the cmake config is broken

--- libs/protobuf/protobuf.py
+++ libs/protobuf/protobuf.py
@@ -16,6 +16,6 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.args += " -Dprotobuf_BUILD_TESTS=OFF"
+        self.subinfo.options.configure.args += " -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_MSVC_STATIC_RUNTIME=OFF"
         if not CraftCore.compiler.isWindows:
             self.subinfo.options.configure.args += " -DBUILD_SHARED_LIBS=ON"
