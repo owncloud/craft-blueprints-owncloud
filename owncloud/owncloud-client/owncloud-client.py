f697379d8926a52cd69273c24ca29471f7eddc6a
Hannah von Reth <hannah.vonreth@owncloud.com>
2020-01-08 13:15:27 +0100

    Add cmocka

--- owncloud/owncloud-client/owncloud-client.py
+++ owncloud/owncloud-client/owncloud-client.py
@@ -37,6 +37,9 @@ class subinfo(info.infoclass):
         if self.buildTarget != "master" and self.buildTarget < CraftVersion("2.6"):
             self.runtimeDependencies["libs/qt5/qtwebkit"] = None
 
+        if self.options.dynamic.buildTests:
+            self.buildDependencies["dev-utils/cmocka"] = None
+
 
 
 from Package.CMakePackageBase import *
