97f0f91d84bfde6610ce797027595a8859ad2499
Hannah von Reth <hannah.vonreth@owncloud.com>
2021-04-10 17:48:12 +0200

    we might depend on glib

--- owncloud/owncloud-client/owncloud-client.py
+++ owncloud/owncloud-client/owncloud-client.py
@@ -190,7 +190,6 @@ class Package(CMakePackageBase):
         self.blacklist.append(re.compile(r"bin[/|\\](?!" + self.applicationExecutable + r").*" + re.escape(CraftCore.compiler.executableSuffix)))
 
         self.ignoredPackages += ["binary/mysql"]
-        self.ignoredPackages += ["libs/glib"]
         if not CraftCore.compiler.isLinux:
             self.ignoredPackages += ["libs/dbus"]
 
