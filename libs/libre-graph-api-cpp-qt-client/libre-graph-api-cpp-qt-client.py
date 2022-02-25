399c9afe9b3bf3d2072fe7c78808d580d8495f10
Hannah von Reth <hannah.vonreth@owncloud.com>
2022-02-25 11:17:47 +0100

    Add libre-graph-api-cpp-qt-client

---
+++ libs/libre-graph-api-cpp-qt-client/libre-graph-api-cpp-qt-client.py
@@ -0,0 +1,23 @@
+import info
+import os
+
+import io
+import re
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets["v0.11.0"] = "https://github.com/owncloud/libre-graph-api-cpp-qt-client.git||v0.11.0"
+        self.targetConfigurePath["v0.11.0"] = "client"
+        self.defaultTarget = "v0.11.0"
+
+        self.description = "Libre Graph Cloud Collaboration API - Qt bindings"
+        self.webpage = "https://owncloud.dev/libre-graph-api/"
+
+    def setDependencies(self):
+        self.buildDependencies["craft/craft-blueprints-owncloud"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
