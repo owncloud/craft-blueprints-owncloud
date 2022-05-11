406124231cd67fcc841968c8a3e8d77834102e2d
Fabian Müller <fmueller@owncloud.com>
2022-05-11 20:35:25 +0200

    Update libappimage

--- libs/_linux/libappimage-minimal/libappimage-minimal.py
+++ libs/_linux/libappimage-minimal/libappimage-minimal.py
@@ -6,16 +6,16 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.description = "Implements functionality for dealing with AppImage files. Minimal build variant."
 
-        for ver in ["1.0.4-3"]:
+        for ver in ["1.0.4-5"]:
             self.targets[ver] = f"https://github.com/AppImage/libappimage/archive/refs/tags/v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"libappimage-{ver}"
 
-        self.targetDigests["1.0.4-3"] = (
-             ["3ed38d08338b66137cf56a3ba3cdd3ef26dfa75dfa6ca02e7faa74f7683dbc12"],
+        self.targetDigests["1.0.4-5"] = (
+             ["2c4e18860a790c5959da6eb064cbd07f165fe6a9b15989491a3a3f176d06af31"],
             CraftHash.HashAlgorithm.SHA256,
         )
 
-        self.defaultTarget = "1.0.4-3"
+        self.defaultTarget = "1.0.4-5"
 
     def setDependencies(self):
         pass
