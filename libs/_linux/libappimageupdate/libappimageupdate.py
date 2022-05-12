b94ddf2bb655f21213d26f4b0e5d82e381158558
Fabian Müller <fmueller@owncloud.com>
2022-05-12 13:27:37 +0200

    Update libappimageupdate

--- libs/_linux/libappimageupdate/libappimageupdate.py
+++ libs/_linux/libappimageupdate/libappimageupdate.py
@@ -6,23 +6,22 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.description = "AppImageUpdate lets you update AppImages in a decentral way using information embedded in the AppImage itself."
 
-        for ver in ["2.0.0-alpha-1-20220124"]:
+        for ver in ["2.0.0-alpha-1-20220512"]:
             self.targets[ver] = f"https://github.com/AppImage/AppImageUpdate/archive/refs/tags/{ver}.tar.gz"
             self.targetInstSrc[ver] = f"AppImageUpdate-{ver}"
 
-        self.targetDigests["2.0.0-alpha-1-20220124"] = (
-            ["d1b9edea302b982fd1d3be4783d3cd6d3ebe0c6c7e284b73abc20925d0e1e246"],
+        self.targetDigests["2.0.0-alpha-1-20220512"] = (
+            ["adbfbb49b6a0ee945ed3cac1909ba9f52173416181aacd7ee1faef844b0c2240"],
             CraftHash.HashAlgorithm.SHA256,
         )
 
-        self.defaultTarget = "2.0.0-alpha-1-20220124"
+        self.defaultTarget = "2.0.0-alpha-1-20220512"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["libs/zsync2"] = None
         self.runtimeDependencies["libs/libappimage-minimal"] = None
         self.runtimeDependencies["libs/cpr"] = None
-        self.runtimeDependencies["libs/argagg"] = None
 
 
 class Package(CMakePackageBase):
@@ -34,4 +33,5 @@ class Package(CMakePackageBase):
             "-DUSE_SYSTEM_LIBAPPIMAGE=ON",
             "-DBUILD_QT_UI=ON",
             "-DBUILD_TESTING=OFF",
+            "-DBUILD_LIBAPPIMAGEUPDATE_ONLY=ON",
         ]
