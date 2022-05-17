bfe4721f5e890159222109d86cbc24453b8f7c54
Fabian Müller <fmueller@owncloud.com>
2022-05-17 12:46:44 +0200

    Update zsync2

--- libs/_linux/zsync2/zsync2.py
+++ libs/_linux/zsync2/zsync2.py
@@ -6,16 +6,16 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.description = "Partial/differential file download client over HTTP(S)"
 
-        for ver in ["2.0.0-alpha-1-20220505"]:
+        for ver in ["2.0.0-alpha-1-20220517"]:
             self.targets[ver] = f"https://github.com/AppImage/zsync2/archive/refs/tags/{ver}.tar.gz"
             self.targetInstSrc[ver] = f"zsync2-{ver}"
 
-        self.targetDigests["2.0.0-alpha-1-20220505"] = (
-            ["43057b5d6ded0736cb3cece5d955f2e6fd3ab04e6f6577292f8bb1f4326af8ac"],
+        self.targetDigests["2.0.0-alpha-1-20220517"] = (
+            ["d213063d6c5c951a394771cc3e700ef9f07ebcbd381480661c29b62f4d4743bf"],
             CraftHash.HashAlgorithm.SHA256,
         )
 
-        self.defaultTarget = "2.0.0-alpha-1-20220505"
+        self.defaultTarget = "2.0.0-alpha-1-20220517"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/cpr"] = None
