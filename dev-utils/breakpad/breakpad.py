9eb850cee9f7660b2fb00f471afd5ffdfc9f6703
Hannah von Reth <hannah.vonreth@owncloud.com>
2023-03-30 13:56:48 +0200

    Use symsorter instead of dump_syms

--- dev-utils/breakpad/breakpad.py
+++ dev-utils/breakpad/breakpad.py
@@ -10,6 +10,9 @@ from Package.MSBuildPackageBase import MSBuildPackageBase
 
 
 class subinfo(info.infoclass):
+    def registerOptions(self):
+        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.Linux
+
     def setTargets(self):
         for ver in ["v2022.07.12"]:
             self.svnTargets[ver] = f"https://chromium.googlesource.com/breakpad/breakpad||{ver}"
@@ -29,96 +32,26 @@ class subinfo(info.infoclass):
             self.buildDependencies["libs/elfutils"] = None
         self.buildDependencies["virtual/base"] = None
 
-    def _fetch(self, package):
+
+class Package(AutoToolsPackageBase):
+    def __init__(self, **args):
+        AutoToolsPackageBase.__init__(self)
+
+    def fetch(self):
         depot_tools = CraftPackageObject.get("dev-utils/depot-tools").instance.sourceDir()
-        srcParent = package.sourceDir().parent
+        srcParent = self.sourceDir().parent
         scriptSuffix = ".bat" if CraftCore.compiler.isWindows else ""
         if not srcParent.exists():
             if not (utils.createDir(srcParent) and utils.system([depot_tools / f"fetch{scriptSuffix}", "--no-history", "breakpad"], cwd=srcParent)):
                 return False
-        if not (utils.system(["git", "clean", "-xdf"], cwd=package.sourceDir()) and utils.system(["git", "reset", "--hard"], cwd=package.sourceDir())):
+        if not (utils.system(["git", "clean", "-xdf"], cwd=self.sourceDir()) and utils.system(["git", "reset", "--hard"], cwd=self.sourceDir())):
             return False
         if not utils.system([depot_tools / f"gclient{scriptSuffix}", "sync", "--revision", self.buildTarget], cwd=srcParent):
             return False
         for patch, lvl in self.patchesToApply():
-            if not utils.applyPatch(package.sourceDir(), package.packageDir() / patch, lvl):
+            if not utils.applyPatch(self.sourceDir(), self.packageDir() / patch, lvl):
                 return False
         return True
 
-    def _configure(self, package):
-        depot_tools = CraftPackageObject.get("dev-utils/depot-tools").instance.sourceDir()
-        srcParent = package.sourceDir().parent
-        scriptSuffix = ".bat" if CraftCore.compiler.isWindows else ""
-        if not utils.system([depot_tools / f"gclient{scriptSuffix}", "runhooks"], cwd=srcParent):
-            return False
+    def unpack(self):
         return True
-
-
-if CraftCore.compiler.isLinux:
-
-    class Package(AutoToolsPackageBase):
-        def __init__(self, **args):
-            AutoToolsPackageBase.__init__(self)
-
-        def fetch(self):
-            return self.subinfo._fetch(self)
-
-        def unpack(self):
-            return True
-
-elif CraftCore.compiler.isWindows:
-
-    class Package(MSBuildPackageBase):
-        def __init__(self, **args):
-            MSBuildPackageBase.__init__(self)
-            self.subinfo.options.configure.projectFile = self.sourceDir() / "src/tools/windows/dump_syms/dump_syms.sln"
-            self.msbuildTargets = ["Clean", "Build"]
-
-        def fetch(self):
-            return self.subinfo._fetch(self)
-
-        def unpack(self):
-            return True
-
-        def configure(self):
-            with utils.ScopedEnv({"GYP_MSVS_VERSION": "2015"}):
-                return self.subinfo._configure(self)
-
-        def install(self):
-            if not BuildSystemBase.install(self):
-                return False
-            dump = glob.glob(str(self.sourceDir() / "src/tools/windows/dump_syms/**/dump_syms.exe"), recursive=True)[0]
-            return utils.copyFile(dump, self.installDir() / "bin/dump_syms.exe")
-
-else:
-
-    class Package(MakeFilePackageBase):
-        def __init__(self, **args):
-            MakeFilePackageBase.__init__(self)
-
-        def fetch(self):
-            return self.subinfo._fetch(self)
-
-        def unpack(self):
-            return True
-
-        def configure(self):
-            return self.subinfo._configure(self)
-
-        def make(self):
-            return utils.system(
-                [
-                    "xcodebuild",
-                    "-configuration",
-                    "Release",
-                    "-target",
-                    "dump_syms",
-                    "-project",
-                    self.sourceDir() / "src/tools/mac/dump_syms/dump_syms.xcodeproj",
-                ]
-            )
-
-        def install(self):
-            if not BuildSystemBase.install(self):
-                return False
-            return utils.copyFile(self.sourceDir() / "src/tools/mac/dump_syms/build/Release/dump_syms", self.installDir() / "bin/dump_syms")
---
+++ dev-utils/symsorter/symsorter.py
@@ -0,0 +1,45 @@
+import stat
+from pathlib import Path
+
+import info
+import utils
+from CraftCore import CraftCore
+from Package.BinaryPackageBase import BinaryPackageBase
+from Utils import CraftHash
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        for ver in ["0.7.0"]:
+            self.targetInstallPath[ver] = "dev-utils/bin"
+            if CraftCore.compiler.isWindows:
+                self.targets[ver] = f"https://github.com/getsentry/symbolicator/releases/download/0.7.0/symsorter-Windows-x86_64.exe"
+                self.targetDigests["0.7.0"] = (["08d9fe1cd902dabdf0f37be88f482bb1cd614e17a98481fe5011288bbe8c09d1"], CraftHash.HashAlgorithm.SHA256)
+            elif CraftCore.compiler.isMacOS:
+                self.targets[ver] = f"https://github.com/getsentry/symbolicator/releases/download/0.7.0/symsorter-Darwin-universal"
+            elif CraftCore.compiler.isLinux:
+                # sysmsorter is a rust binary and we need a build on centos7...
+                self.targets[ver] = f"https://cloud.owncloud.com/index.php/s/xgH54K9Yw04b0Vg/download"
+                self.archiveNames[ver] = "symsorter.7z"
+                self.targetDigests["0.7.0"] = (["4665df5b6f0d2b59f28deae5ffebe68de677406b0c965b87ea6d54940187614d"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "0.7.0"
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+
+
+class Package(BinaryPackageBase):
+    def __init__(self, **args):
+        BinaryPackageBase.__init__(self)
+
+    def install(self):
+        if not super().install():
+            return False
+        dest = self.installDir() / f"symsorter{CraftCore.compiler.executableSuffix}"
+        if not dest.exists():
+            # in case we directly downloaded a binary move it
+            if not utils.copyFile(self.sourceDir() / Path(self.subinfo.target()).name, dest):
+                return False
+        if CraftCore.compiler.isUnix:
+            dest.chmod(dest.stat().st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+        return True
