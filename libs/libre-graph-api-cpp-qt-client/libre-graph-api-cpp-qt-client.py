09ef3df53adc50a430971b4437595c0f232b6909
Hannah von Reth <hannah.vonreth@owncloud.com>
2023-05-02 14:45:52 +0200

    Update libre-graph-api-cpp-qt-client 1.0.3

--- libs/libre-graph-api-cpp-qt-client/libre-graph-api-cpp-qt-client.py
+++ libs/libre-graph-api-cpp-qt-client/libre-graph-api-cpp-qt-client.py
@@ -10,11 +10,17 @@ class subinfo(info.infoclass):
         for ver in ["v0.11.0", "v0.12.0", "v0.13.2", "v1.0.1"]:
             self.svnTargets[ver] = f"https://github.com/owncloud/libre-graph-api-cpp-qt-client.git||{ver}"
             self.targetConfigurePath[ver] = "client"
-            self.defaultTarget = ver
+
+        for ver in ["1.0.3"]:
+            self.targets[ver] = f"https://github.com/owncloud/libre-graph-api-cpp-qt-client/archive/refs/tags/v{ver}.tar.gz"
+            self.targetConfigurePath[ver] = "client"
+            self.targetInstSrc[ver] = f"libre-graph-api-cpp-qt-client-{ver}"
+        self.targetDigests["1.0.3"] = (["17cd8b03f5fca97d9944701f183dfd44d88c3a00a01ed0395aa925768551d16c"], CraftHash.HashAlgorithm.SHA256)
 
         self.svnTargets["main"] = f"https://github.com/owncloud/libre-graph-api-cpp-qt-client.git|main|"
         self.targetConfigurePath["main"] = "client"
 
+        self.defaultTarget = "1.0.3"
         self.description = "Libre Graph Cloud Collaboration API - Qt bindings"
         self.webpage = "https://owncloud.dev/libre-graph-api/"
 
