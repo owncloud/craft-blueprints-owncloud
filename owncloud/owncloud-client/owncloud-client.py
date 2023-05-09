295ddf694ed37bfc3720a7eded865716eb5318fe
Hannah von Reth <hannah.vonreth@owncloud.com>
2023-05-09 08:24:11 +0200

    libregraph is a runtime dep

--- owncloud/owncloud-client/owncloud-client.py
+++ owncloud/owncloud-client/owncloud-client.py
@@ -37,8 +37,8 @@ class subinfo(info.infoclass):
         self.buildDependencies["craft/craft-blueprints-owncloud"] = None
         self.buildDependencies["dev-utils/cmake"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
-        self.buildDependencies["libs/libre-graph-api-cpp-qt-client"] = None
-
+        
+        self.runtimeDependencies["libs/libre-graph-api-cpp-qt-client"] = None
         self.runtimeDependencies["libs/sparkle"] = None
         self.runtimeDependencies["libs/zlib"] = None
         self.runtimeDependencies["libs/sqlite"] = None
